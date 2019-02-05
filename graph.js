35666646e1106634de7a44d40b0d04304a269f07
Eugene Schava <eschava@gmail.com>
2019-02-05 12:16:20 +0200

    graph should be case sensitive by default

--- graph.js
+++ graph.js
@@ -309,7 +309,7 @@ var contextMenus = {
 };
 
 var appState = {
-    graph: new fbpGraph.Graph(name = '', options = {caseSensitive: true}),
+    graph: new fbpGraph.Graph('', {caseSensitive: true}),
     library: {},
     theme: 'light',
 };
